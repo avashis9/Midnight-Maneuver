f7d2be5a50dee5c07f8ad6656fb6fc5f988dd862
avashis9 <avashis9@asu.edu>
2024-10-28 12:56:56 -0700

    imported assets

---
+++ Midnight-Maneuver/Assets/Stylized NPC - Peasant Nolant/README.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:17c9b8e7e190ce32bfc103ea4e1c0215ad691c1dd802bc2dc6fe1a67f8c7d211
+size 284
---
+++ Midnight-Maneuver/Assets/TeKniKo_Free/Power_Ups/Scripts/ObjectBounce.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1943aa751f176ccec5ccb0683db5398a2b75b561acbe91ff91c3665abb49767e
+size 1073
